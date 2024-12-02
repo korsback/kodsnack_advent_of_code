1ee3d68bb1c8b0aa2207cf309fb9888f629c4310
Erik Dyrelius <erik.dyrelius@gmail.com>
2024-12-02 06:44:17 +0100

    Day 2 solved in python

--- 2024/Dyr-El-python/d2024_02.py
+++ 2024/Dyr-El-python/d2024_02.py
@@ -1,20 +1,75 @@
 from aoc_prepare import PrepareAoc
 
 
+def parseLevels(inp):
+    return [[int(x) for x in line.split()] for line in inp.splitlines()]
+
+
+def checkLevel(level):
+    state = "="
+    lastDigit = None
+    for digit in level:
+        if state == "=":
+            if lastDigit is None:
+                pass
+            elif lastDigit > digit >= lastDigit - 3:
+                state = "-"
+            elif lastDigit < digit <= lastDigit + 3:
+                state = "+"
+            else:
+                return False
+        elif state == "+" and not (lastDigit < digit <= lastDigit + 3):
+            return False
+        elif state == "-" and not (lastDigit > digit >= lastDigit - 3):
+            return False
+        lastDigit = digit
+    return True
+
+
 def part1(inp):
-    pass
+    return sum((checkLevel(level) for level in parseLevels(inp)))
 
 
 def part2(inp):
-    pass
+    return sum(
+        (
+            any(
+                (
+                    checkLevel(level[:idx] + level[idx + 1 :])
+                    for idx in range(len(level))
+                )
+            )
+            for level in parseLevels(inp)
+        )
+    )
 
 
 def test_1_1():
-    pass
+    assert (
+        part1(
+            """7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9"""
+        )
+        == 2
+    )
 
 
 def test_1_2():
-    pass
+    assert (
+        part2(
+            """7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9"""
+        )
+        == 4
+    )
 
 
 def main(inp):
