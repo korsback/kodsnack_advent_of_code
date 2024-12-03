1461116b33d900a77d6b445d99ddf8bf4a1e41ab
Meconer <mats.asson@gmail.com>
2024-12-03 15:41:23 +0100

    Day 3 completed

--- 2024/meconer-ocaml/bin/main.ml
+++ 2024/meconer-ocaml/bin/main.ml
@@ -2,3 +2,5 @@ let _ = Printf.printf "Day1 part 1 : %d\n" ( Day1.resultP1);
 Printf.printf "Day1 part 2 : %d\n" ( Day1.resultP2);
 Printf.printf "Day2 part 1 : %d\n" ( Day2.resultP1);
 Printf.printf "Day2 part 2 : %d\n" ( Day2.resultP2);
+Printf.printf "Day3 part 1 : %d\n" ( Day3.resultP1);
+Printf.printf "Day3 part 2 : %d\n" ( Day3.resultP2);
